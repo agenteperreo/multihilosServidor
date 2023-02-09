9454b9a1e8a326bc82530121a79ebc4f59981447
Isaac <igarcar@iesnervion.es>
2023-02-09 19:18:25 +0100

    09/02/2023

--- src/ejercicio1/Cliente.java
+++ src/ejercicio1/Cliente.java
@@ -9,11 +9,20 @@ import java.util.Scanner;
 
 public class Cliente {
     public static void main(String[] args) {
-        DatagramSocket socket = null;
+        //Creamos el socket para conectarnos
+        DatagramSocket socket;
+
+        //Puerto mediante el que nos vamos a conectar al servidor
         int puertoServidor = 41600;
+
+        //Nombre del servidor
         String nombreServidor = "localhost";
+
+        //Abrimos el escaner
         Scanner sc = new Scanner(System.in);
-        String numeroUsuario;
+
+        //Numero que el usuario cree que es el numero oculto, respuesta que envia el servidor simplificada con trim
+        String numeroUsuario, respuestaServidor="";
         try {
             // 1 - Obtención de la dirección del servidor usando el métod getByName de
             // InetAddress
@@ -24,32 +33,47 @@ public class Cliente {
             System.out.println("(Cliente): Creando el socket...");
             socket = new DatagramSocket();
 
+
             // 3 - Generación del datagrama
             System.out.println("(Cliente): Creando datagrama...");
-            System.out.print("Dime el numero que creas que es el oculto: ");
-            numeroUsuario = sc.nextLine();
 
-            byte[] bufferSalida = numeroUsuario.getBytes();
-            DatagramPacket paqueteSalida = new DatagramPacket(bufferSalida, bufferSalida.length, direccionServidor,
-                    puertoServidor);
+            //Mientras la respuesta del servidor no sea "¡¡¡Has acertado!!!"
+            while(!respuestaServidor.equals("¡¡¡Has acertado!!!")) {
 
-            // 4 - Envío del datagrama a través de send
-            System.out.println("(Cliente) Enviando datagrama...");
-            socket.send(paqueteSalida);
+                //Le pedimos un numero al usuario
+                System.out.print("Dime el numero que creas que es el oculto: ");
+                numeroUsuario = sc.nextLine();
 
-            // 5 - Recepción de la respuesta
-            System.out.println("(Cliente) Recibiendo respuesta...");
-            byte[] bufferEntrada = new byte[64];
-            DatagramPacket paqueteEntrada = new DatagramPacket(bufferEntrada, bufferEntrada.length, direccionServidor,
-                    puertoServidor);
-            socket.receive(paqueteEntrada);
-            System.out.println("Mensaje recibido: " + new String(bufferEntrada));
+                //Creamos el array de bytes con el que enviaremos el numero
+                byte[] bufferSalida = numeroUsuario.getBytes();
 
+                //Creamos el datagrama que vamos a enviar
+                DatagramPacket paqueteSalida = new DatagramPacket(bufferSalida, bufferSalida.length, direccionServidor,
+                        puertoServidor);
+
+                // 4 - Envío del datagrama a través de send
+                System.out.println("(Cliente) Enviando datagrama...");
+                socket.send(paqueteSalida);
+
+                // 5 - Recepción de la respuesta
+                System.out.println("(Cliente) Recibiendo respuesta...");
+                byte[] bufferEntrada = new byte[64];
+                DatagramPacket paqueteEntrada = new DatagramPacket(bufferEntrada, bufferEntrada.length, direccionServidor,
+                        puertoServidor);
+                socket.receive(paqueteEntrada);
+
+                //Guardamos la respuesta del servidor en la variable creada anteriormente
+                respuestaServidor = new String(bufferEntrada).trim();
+
+                //Mostramos la respuesta del servidor
+                System.out.println("Mensaje recibido: " + respuestaServidor);
+            }
             // 6 - Cierre del socket
             System.out.println("(Cliente): Cerrando conexión...");
             socket.close();
             System.out.println("(Cliente): Conexión cerrada.");
 
+        //Control de excepciones
         } catch (SocketException e) {
             System.err.println("Error al conectar con el servidor.");
             e.printStackTrace();
--- src/ejercicio1/GestorDeProcesos.java
+++ src/ejercicio1/GestorDeProcesos.java
@@ -5,54 +5,68 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 
 public class GestorDeProcesos extends Thread {
+
+    //Creamos la variable en la que guardaremos el numero oculto
     static int numeroAleatorio = (int) (Math.random() * 100) + 1;
-    static boolean acertado = false;
+
+    //Socket para conectarnos con el cliente
     DatagramSocket socket;
+
+    //Datagrama que nos enviará el cliente
     DatagramPacket datagramaEntrada;
 
+    //Constructor de la clase
     public GestorDeProcesos(DatagramSocket socket, DatagramPacket datagramaEntrada) {
         super();
         this.socket = socket;
         this.datagramaEntrada = datagramaEntrada;
     }
 
+    //Metodo run que ejecutara cada hilo
     @Override
     public void run() {
+
+        //Llamamos a la funcion realizarProceso en la que se daran las pistas que le da al cliente
         realizarProceso();
     }
 
     public void realizarProceso() {
-        while (!acertado) {
 
-            // Recepción de mensaje del cliente
-            String mensajeRecibido = new String(datagramaEntrada.getData());
-            System.out.println("(Servidor): Mensaje recibido: " + mensajeRecibido);
+        // Recepción de mensaje del cliente
+        String mensajeRecibido = new String(datagramaEntrada.getData());
+        System.out.println("(Servidor): Mensaje recibido: " + mensajeRecibido.trim());
 
-            //Parseamos la respuesta del cliente
-            int respCliente = Integer.parseInt(mensajeRecibido);
+        //Parseamos la respuesta del cliente
+        int respCliente = Integer.parseInt(mensajeRecibido.trim());
 
-            // 5 - Generación y envío de la respuesta
-            System.out.println("(Servidor): Enviando datagrama...");
-            byte[] mensajeEnviado;
+        // 5 - Generación y envío de la respuesta
+        System.out.println("(Servidor): Enviando datagrama...");
+        byte[] mensajeEnviado;
 
-            if (respCliente > numeroAleatorio) {
-                mensajeEnviado = new String("El numero es menor").getBytes();
-            } else if (respCliente < numeroAleatorio) {
-                mensajeEnviado = new String("El numero es mayor").getBytes();
-            } else {
-                mensajeEnviado = new String("¡¡¡Has acertado!!!").getBytes();
-                acertado = true;
-            }
+        //Si la respuesta del cliente es mayor que el numero oculto
+        if (respCliente > numeroAleatorio) {
+            //La respuesta "El numero es menor" se guarda en el array de bytes
+            mensajeEnviado = new String("El numero es menor").getBytes();
+        //Si la respuesta del usuario es menor que el numero oculto
+        } else if (respCliente < numeroAleatorio) {
+            //La respuesta "El numero es mayor" se guarda en el array de bytes
+            mensajeEnviado = new String("El numero es mayor").getBytes();
+        //Si no
+        } else {
+            //La respuesta "¡¡¡Has acertado!!!" se guarda en el array de bytes
+            mensajeEnviado = new String("¡¡¡Has acertado!!!").getBytes();
+        }
 
-            DatagramPacket packetSalida = new DatagramPacket(mensajeEnviado, mensajeEnviado.length, datagramaEntrada.getAddress(), datagramaEntrada.getPort());
+        //Se crea el datagrama que se va a enviar mediante el array de bytes
+        DatagramPacket packetSalida = new DatagramPacket(mensajeEnviado, mensajeEnviado.length, datagramaEntrada.getAddress(), datagramaEntrada.getPort());
 
-            try {
-                socket.send(packetSalida);
-            } catch (IOException e) {
-                System.err.println("ERROR: Algo sucedió en el envio del paquete");
-                System.err.println(e.getMessage());
-                e.printStackTrace();
-            }
+        try {
+            //Enviamos el datagrama
+            socket.send(packetSalida);
+        } catch (IOException e) {
+            System.err.println("ERROR: Algo sucedió en el envio del paquete");
+            System.err.println(e.getMessage());
+            e.printStackTrace();
         }
     }
 }
